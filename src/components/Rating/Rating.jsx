42d80aa0bc435f1b1a22112b33b4e457eceb5f2a
alireza <rezaei03a@gmail.com>
2023-09-29 01:36:27 +0330

    feat: add tailwind to project

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Rating() {
     return (
-        <div>Rating</div>
+        <div className='text-sm bg-slate-400 rounded-lg px-4 py-1 inline-flex text-white mx-4'>Rating</div>
     )
 }
