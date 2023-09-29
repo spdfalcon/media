d254711f4258bb8559a86b67b6bf1e23e0686ab7
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-28 22:42:50 -0700

    add font and color in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'main-red-admin': '#E20E02',
+                'main-orang-admin': '#F68A04',
+                'main-blue-admin': '#007AFF',
+                'main-green-admin': '#14E788',
+                'main-gray-admin': '#858EA3',
+                'main-gray-text-admin': '#d1d0cf',
+        
+                'main-orang-web' : '#FF9800',
+                'main-bg-web-dark' : '#0D121E',
+                'second-bg-web-dark' : '#1A1F2A',
+                'third-web-dark' : '#2E313A',
+                'main-green-web' : '#0CC13F',
+              },
+              fontFamily:{
+                iransans : ['IRANSans']
+              }
+        },
     },
     plugins: [],
 }
