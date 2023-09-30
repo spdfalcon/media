158f77d4b06de8d72eae0d3a019df656998e8425
alireza <rezaei03a@gmail.com>
2023-09-30 12:03:16 +0330

    fix: fix main page bug

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -4,8 +4,11 @@ import TopBar from '../../components/TopBar/TopBar'
 
 export default function Admin() {
     return (
-        <div className='flex flex-col relative'>
+        <div className='flex flex-col relative ltr-theme'>
             <TopBar />
+            <div className="p-main rtl-theme">
+                ممد جون این دیو برا توعه
+            </div>
         </div>
     )
 }
